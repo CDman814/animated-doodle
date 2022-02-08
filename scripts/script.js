64c6c9648a7680d40a18a0c24dc742fbe7df3f4b
CDman814 <81450644+CDman814@users.noreply.github.com>
2022-02-07 23:10:18 -0600

    Update script.js

--- scripts/script.js
+++ scripts/script.js
@@ -30,7 +30,6 @@ function generateCoolNameFacts(userName) {
   coolFacts = nameLength(userName);
   coolFacts += " <br> " + nameStart(userName);
   coolFacts += "<h3> " + nameEnd(userName) + "</h3>";
-  coolFacts += "<h2>" + reverseName(userName) + "</h2>";
   coolFacts += findSpiritAnimal(userName);
 
   console.log("initialized the'm coolFacts variable: " + coolFacts); 
